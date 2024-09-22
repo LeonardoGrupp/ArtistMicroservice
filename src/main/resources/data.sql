171fe4c50ba3a8b10287f230ed4d535a35704190
Fredrik Lundell <fredrik_lundell@icloud.com>
2024-09-23 00:47:37 +0200

    adding more artists

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,36 +1,85 @@
 INSERT INTO artist (name)
 VALUES
     -- MUSIC
-    ('In Flames'),
-    ('Lisa Miskovsky'),
-    ('Rihanna'),
-    ('Jay-Z'),
-    ('Jeezy'),
-    ('Slash'),
-    ('Will.I.Am'),
-    ('Ne-Yo'),
+    ('In Flames'), -- 1
+    ('Lisa Miskovsky'), -- 2
+    ('Rihanna'), -- 3
+    ('Jay-Z'), -- 4
+    ('Jeezy'), -- 5
+    ('Slash'), -- 6
+    ('Will.I.Am'), -- 7
+    ('Ne-Yo'), -- 8
 
     -- VIDEO
-    ('Iron Maiden'),
-    ('Dire Straits'),
+    ('Iron Maiden'), -- 9
+    ('Dire Straits'), -- 10
 
     -- POD
-    ('Joe Rogan'),
-    ('Elon Musk'),
-    ('Quentin Tarantino'),
-    ('Kevin Hart'),
-    ('Neil deGrasse Tyson'),
+    ('Joe Rogan'), -- 11
+    ('Elon Musk'), -- 12
+    ('Quentin Tarantino'), -- 13
+    ('Kevin Hart'), -- 14
+    ('Neil deGrasse Tyson'), -- 15
 
-    ('Lex Fridman'),
-    ('Tucker Carlson'),
-    ('Kanye West'),
-    ('Paul Rosolie'),
+    ('Lex Fridman'), -- 16
+    ('Tucker Carlson'), -- 17
+    ('Kanye West'), -- 18
+    ('Paul Rosolie'), -- 19
 
-    ('Theo Von'),
-    ('Bobby Lee'),
-    ('KSI'),
-    ('Druski'),
-    ('Post Malone')
+    ('Theo Von'), -- 20
+    ('Bobby Lee'), -- 21
+    ('KSI'), -- 22
+    ('Druski'), -- 23
+    ('Post Malone'), -- 24
+
+    -- Music
+    ('D''Angelo'), -- 25
+    ('Method Man'), -- 26
+    ('Redman'), -- 27
+    ('Frank Ocean'), -- 28
+    ('Earl Sweatshirt'), -- 29
+    ('John Mayer'), -- 30
+    ('André 3000'), -- 31
+    ('Lauryn Hill'), -- 32
+    ('Carlos Santana'), -- 33
+    ('Mary J. Blige'), -- 34
+    ('Maxwell'), -- 35
+    ('Marvin Gaye'), -- 36
+    ('Janelle Monae'), -- 37
+    ('Prince'), -- 38
+    ('Erykah Badu'), -- 39
+    ('Solange'), -- 40
+    ('Miguel'), -- 41
+    ('Esperanza Spalding'), -- 42
+    ('Usher'), -- 43
+    ('Lil John'), -- 44
+    ('Ludacris'), -- 45
+    ('John Legend'), -- 46
+    ('Snoop Dogg'), -- 47
+    ('The Stephens Family'), -- 48
+    ('Miri Ben-Ari'), -- 49
+    ('Porter Robinson'), -- 50
+    ('Zedd'), -- 51
+    ('Bahari'), -- 52
+    ('Selena Gomez'), -- 53
+    ('Jon Bellion'), -- 54
+    ('Logic'), -- 55
+    ('X Ambassadors'), -- 56
+    ('Troye Sivan'), -- 57
+    ('Echosmith'), -- 58
+    ('Hayley Williams'), -- 59
+    ('Matthew Koma'), -- 60
+    ('Miriam Bryant'), -- 61
+    ('Foxes'), -- 62
+    ('Avicii'), -- 63
+    ('Madeon'), -- 64
+    ('Amy Millan'), -- 65
+    ('Breanne Düren'), -- 66
+    ('Sean Caskey'), -- 67
+    ('Lemaitre'), -- 68
+    ('Imaginary Cities'), -- 69
+    ('Urban Cone'), -- 70
+    ('Kygo') -- 71
 
     -- VIDEO
 --     (''),
